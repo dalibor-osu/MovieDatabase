0c69bf6f246d3ed55f89d5ee86a40fcef54c75a5
Dalibor <drevojanekdalibor@gmail.com>
2023-05-03 09:22:53 +0200

    Remove comments

--- src/UserControl/MenuPrinter.java
+++ src/UserControl/MenuPrinter.java
@@ -5,18 +5,18 @@ import static java.lang.System.out;
 public abstract class MenuPrinter {
     public static void printMainMenu() {
         out.println("Choose action from menu:");
-        out.println("\t1 ... Add new movie"); // done
-        out.println("\t2 ... Edit movie"); // done
-        out.println("\t3 ... Delete movie"); // done
-        out.println("\t4 ... Add movie rating"); // done
-        out.println("\t5 ... Search movie"); // done
-        out.println("\t6 ... Print all movies"); // done
-        out.println("\t7 ... Print staff members who worked on more than 1 movie"); // done
-        out.println("\t8 ... Print movies with certain staff member"); // done
-        out.println("\t9 ... Save movie info to file"); // done
-        out.println("\t10... Save all movies to directory"); // done
-        out.println("\t11... Load movie from file"); // done
-        out.println("\t12... Exit program"); // done
+        out.println("\t1 ... Add new movie");
+        out.println("\t2 ... Edit movie");
+        out.println("\t3 ... Delete movie");
+        out.println("\t4 ... Add movie rating");
+        out.println("\t5 ... Search movie");
+        out.println("\t6 ... Print all movies");
+        out.println("\t7 ... Print staff members who worked on more than 1 movie");
+        out.println("\t8 ... Print movies with certain staff member");
+        out.println("\t9 ... Save movie info to file");
+        out.println("\t10... Save all movies to directory");
+        out.println("\t11... Load movie from file");
+        out.println("\t12... Exit program");
     }
 
     public static void printEditMenu() {
